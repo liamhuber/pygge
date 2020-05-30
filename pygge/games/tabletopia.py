c4aa8d64c0720061e5727e070df670e80de54a1f
Liam Huber <liam.huber@gmail.com>
2020-05-30 10:23:25 -0700

    Add tabletopia game pieces

--- pygge/games/tabletopia.py
+++ pygge/games/tabletopia.py
@@ -3,6 +3,8 @@
 # Distributed under the terms of "New BSD License", see the LICENSE file.
 
 from PIL import Image, ImageDraw
+from .. shapes import Shape, Hex, Rectangle, Ellipse
+from abc import abstractmethod
 
 """
 Methods exclusive to the [tabletopia](www.tabletopia.com) platform
@@ -42,3 +44,43 @@ def magnetic_map(shape, color='green', orientable=True, endpoint='n'):
         end = start + vector  # TODO: Double check you don't need to reverse y coord
         draw.line(start.inttuple + end.inttuple, fill='purple', width=1)
     return shape.cut_out_shape(map_)
+
+
+class Piece2d(Shape):
+    """
+    TODO: Borders
+    """
+
+    def __init__(self, size=None, background=None, **graphic_kwargs):
+        super().__init__(size=size or self.default_size, **graphic_kwargs)
+        if background is not None:
+            self.add_background('background', content=background)
+
+    @property
+    @abstractmethod
+    def default_size(self):
+        raise NotImplemented
+
+
+class HexTile(Hex, Piece2d):
+    @property
+    def default_size(self):
+        return 440, 385
+
+
+class Card(Rectangle, Piece2d):
+    @property
+    def default_size(self):
+        return 440, 624
+
+
+class Counter(Ellipse, Piece2d):
+    @property
+    def default_size(self):
+        return 244, 228
+
+
+class TokenHorizontal(Ellipse, Piece2d):
+    @property
+    def default_size(self):
+        return 440, 444
