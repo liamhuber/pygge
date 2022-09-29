09ddd5223fe64dabd64b34b3b7e7dabbfc6cc2a8
Liam Huber <liam.huber@gmail.com>
2022-09-29 10:44:45 -0700

    Note wrap failure in manual test

--- tests/test_graphic.py
+++ tests/test_graphic.py
@@ -16,10 +16,16 @@ class TestGraphic(TestCase):
         g.add('title', title)  #?
 
     def test(self):
-        g = Graphic(size=(50, 100), color="white")
-        g.add('green', Graphic(size=(20, 20), position=(-10, -10), parent=g, color="green"))
-        g.add('red', Graphic(size=(20, 20), position=(40, 90), parent=g, color="red"))
-        g.text.text = "Foobar"
+        g = Graphic(size=(50, 100), color="white", text="FooBarBazBoa", text_color="blue")
+        # Text is not wrapping/shrinking!
+
         g.text.position = (g.text.position.x, 0.5 * g.size.y)
+
+        green_graphic = Graphic(size=(20, 20), position=(-10, -10), color="green")
+        g.add('green', green_graphic)
+
+        g.add('red', Graphic(size=(20, 20), position=(40, 90)))
+        g.red.color = "red"
+
         g.image.show()
 
